48f4b8ef916706fd3833fc4bc5e4bc0cdcbee151
Jasonyu77 <yujiexun77@gmail.com>
2024-06-14 12:10:14 +0930

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,7 +1,8 @@
-git checkout -b branch2' > question11.sh
+git checkout -b branch2
 echo "Initial content" > file4
 git add file4
 git commit -m "Add file4"
-echo "Modified content" >> file4
-git stash push -m "Stash changes to file4"
+echo "Modified content"
+git add file4
+git stash
 git checkout main
